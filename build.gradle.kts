80eab23a56c4211343489db3d9729cd1ff953fd6
Karox1234 <zzzcomet@gmail.com>
2024-02-04 19:11:30 +0900

    Feat : 메일인증 구현을 위해 gradle 추가

--- build.gradle.kts
+++ build.gradle.kts
@@ -37,10 +37,11 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-aop")
 	implementation("org.springframework.boot:spring-boot-starter-security")
 	implementation("io.jsonwebtoken:jjwt-api:0.12.3")
+	implementation ("org.springframework.boot:spring-boot-starter-mail")
 
 	runtimeOnly("io.jsonwebtoken:jjwt-impl:0.12.3")
 	runtimeOnly("io.jsonwebtoken:jjwt-jackson:0.12.3")
-	//implementation("com.h2database:h2")
+//	implementation("com.h2database:h2")
 
 	runtimeOnly("org.postgresql:postgresql")
 }
