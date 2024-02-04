ef379101fe343e446252c3312aa918bac4e18bac
Karox1234 <zzzcomet@gmail.com>
2024-02-04 19:14:24 +0900

    Feat: 스케쥴러 어노테이션 추가

--- src/main/kotlin/com/teamsparta/todoapp/ToDoAppApplication.kt
+++ src/main/kotlin/com/teamsparta/todoapp/ToDoAppApplication.kt
@@ -3,8 +3,10 @@ package com.teamsparta.todoapp
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 import org.springframework.context.annotation.EnableAspectJAutoProxy
+import org.springframework.scheduling.annotation.EnableScheduling
 
 
+@EnableScheduling
 @EnableAspectJAutoProxy
 @SpringBootApplication
 class ToDoAppApplication
