90494855d33733e92a8c80d13f6e65668bf97e2a
Karox1234 <zzzcomet@gmail.com>
2024-02-13 01:01:22 +0900

    Feat: comment 페이징

--- src/main/kotlin/com/teamsparta/todoapp/domain/comment/repository/CommentRepository.kt
+++ src/main/kotlin/com/teamsparta/todoapp/domain/comment/repository/CommentRepository.kt
@@ -2,8 +2,12 @@ package com.teamsparta.todoapp.domain.comment.repository
 
 
 import com.teamsparta.todoapp.domain.comment.model.Comment
+import org.springframework.data.domain.Page
+import org.springframework.data.domain.Pageable
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface CommentRepository : JpaRepository<Comment, Long> {
     fun getCommentsByCardId(cardId: Long): List<Comment>
+
+    fun findByCardId(cardId: Long, pageable: Pageable): Page<Comment>
 }
