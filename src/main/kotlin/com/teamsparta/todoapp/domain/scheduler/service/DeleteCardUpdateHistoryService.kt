17cf4674bad094c4b196d6d8a88528628d44dfd4
Karox1234 <zzzcomet@gmail.com>
2024-02-12 20:37:15 +0900

    Fix : 변경이력 스케쥴러 완성

--- src/main/kotlin/com/teamsparta/todoapp/domain/scheduler/service/DeleteCardUpdateHistoryService.kt
+++ src/main/kotlin/com/teamsparta/todoapp/domain/scheduler/service/DeleteCardUpdateHistoryService.kt
@@ -11,12 +11,9 @@ class DeleteCardUpdateHistoryService(private val revisionRepository: RevisionRep
     @Transactional
     fun deleteCardUpdateHistory() {
         val currentTimestamp = LocalDateTime.now()
+        val timestamp90DaysAgo = currentTimestamp.minusDays(90)
 
-        // 현재 시간보다 이전에 생성된 리비전 삭제
-        revisionRepository.deleteByTimestampBefore(currentTimestamp)
+        revisionRepository.deleteByTimestampBefore(timestamp90DaysAgo)
     }
 }
-//원래 의도는 리비전info를 삭제하면서 연결된 aud들을 다 삭제 하려고 했는데, 키 제약 조건때문에 삭제가 안됨
-//TODO: 키 제약 조건 해결후 삭제가 되면, 시간 조건 걸기(90일)
-
 
