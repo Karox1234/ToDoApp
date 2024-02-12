6c28279802330c12b23c7439dafdfe54b3b65e19
Karox1234 <zzzcomet@gmail.com>
2024-02-13 01:05:01 +0900

    Fix : fetch타입 EAGER로 변경

--- src/main/kotlin/com/teamsparta/todoapp/domain/cards/model/Card.kt
+++ src/main/kotlin/com/teamsparta/todoapp/domain/cards/model/Card.kt
@@ -1,6 +1,9 @@
 package com.teamsparta.todoapp.domain.cards.model
 
+import com.teamsparta.todoapp.domain.cards.dto.CardAndCommentResponse
 import com.teamsparta.todoapp.domain.cards.dto.CardResponse
+import com.teamsparta.todoapp.domain.comment.dto.CommentResponse
+import com.teamsparta.todoapp.domain.comment.model.Comment
 import com.teamsparta.todoapp.domain.image.model.Image
 import com.teamsparta.todoapp.domain.user.model.UserEntity
 import com.teamsparta.todoapp.infra.BaseTimeEntity
@@ -8,6 +11,7 @@ import jakarta.persistence.*
 import org.hibernate.annotations.OnDelete
 import org.hibernate.annotations.OnDeleteAction
 import org.hibernate.envers.Audited
+import org.hibernate.envers.NotAudited
 
 @Audited
 @Entity
@@ -22,7 +26,7 @@ class Card(
     @Column(name = "completed") var completed: Boolean = false,
 
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = FetchType.EAGER)
     @JoinColumn(name = "user_id")
     @OnDelete(action = OnDeleteAction.CASCADE)
     val user: UserEntity,
@@ -31,7 +35,11 @@ class Card(
     var imageUrl: String?,
 
     @OneToMany(mappedBy = "card", cascade = [CascadeType.ALL], orphanRemoval = true)
-    val images: MutableList<Image> = mutableListOf()
+    val images: MutableList<Image> = mutableListOf(),
+
+    @NotAudited
+    @OneToMany(mappedBy = "card", fetch = FetchType.EAGER)
+    val comments: List<Comment> = mutableListOf()
 
 ) : BaseTimeEntity() {
     @Id
@@ -50,5 +58,20 @@ fun Card.toResponse(): CardResponse {
         userId = user.id!!,
         completed = completed,
         imageUrl = imageUrl
+    )
+}
+
+fun Card.toCardAndCommentResponse(commentResponses: List<CommentResponse>): CardAndCommentResponse {
+    return CardAndCommentResponse(
+        id = id!!,
+        title = title,
+        description = description,
+        writer = user.profile.nickname,
+        createdAt = createdAt,
+        updatedAt = updatedAt,
+        userId = user.id!!,
+        completed = completed,
+        imageUrl = imageUrl,
+        comments = commentResponses,
     )
 }
